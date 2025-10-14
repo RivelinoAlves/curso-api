73eb30262c1588e6beee4af2dd9c8e5638e47698
“Rivelino <rivaling2010medina@gmail.com>
2025-10-14 09:11:47 -0300

    Projeto inicial

---
+++ src/main/java/br/com/rivelino/demo/ApiudemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.rivelino.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiudemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiudemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rivelino/demo/ApiudemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rivelino.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiudemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

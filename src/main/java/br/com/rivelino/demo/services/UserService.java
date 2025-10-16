6f154a2744d4da99492791626b7a33c3bf38d8d0
“Rivelino <rivaling2010medina@gmail.com>
2025-10-16 10:19:53 -0300

    Finda all for user

--- src/main/java/br/com/rivelino/demo/services/UserService.java
+++ src/main/java/br/com/rivelino/demo/services/UserService.java
@@ -2,6 +2,9 @@ package br.com.rivelino.demo.services;
 
 import br.com.rivelino.demo.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     User findById(Integer id);
+    List<User> findAll();
 }

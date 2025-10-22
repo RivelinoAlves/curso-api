716191146fdeb8e99472da08aa24d2042d6b3120
“Rivelino <rivaling2010medina@gmail.com>
2025-10-22 10:53:30 -0300

    classe para testes userresources iniciada

---
+++ src/test/java/br/com/rivelino/demo/resources/UserResourceTest.java
@@ -0,0 +1,64 @@
+package br.com.rivelino.demo.resources;
+
+
+import br.com.rivelino.demo.domain.User;
+import br.com.rivelino.demo.domain.dto.UserDTO;
+import br.com.rivelino.demo.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserResourceTest {
+    private static final Integer ID      = 1;
+    private static final String EMAIL    = "roberto@teste.com";
+    private static final String PASSWORD = "roberto123";
+    private static final String NAME     = "roberto";
+
+    private User user;
+    private UserDTO userDTO;
+
+    @InjectMocks
+    private UserResource resource;
+
+    @Mock
+    private UserServiceImpl service;
+
+    @Mock
+    private ModelMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+    }
+}
